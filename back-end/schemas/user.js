d4c865749f7d4a8c38775bf713c2a2266ebc2d19
Mahak Rawat <mahakrawat19@gmail.com>
2022-04-23 02:34:08 +0530

    user schema added

---
+++ back-end/schemas/user.js
@@ -0,0 +1,93 @@
+const mongoose=require('mongoose')
+const validator= require('validator')
+const bcrypt=require('bcrypt')
+const jwt= require('jsonwebtoken')
+
+const userschema= mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    age:{
+       type: Number,
+       default: 0 
+    },
+    blood_group:{
+      type:String,
+      required:true,
+    },
+    password:{
+        type: String,
+        required: true,
+        trim:true,
+        minLength:7,
+        validate(value){
+          if(value.toLowerCase().includes('password'))
+          {throw new Error('invalid password');}
+          }
+    },
+    email:{
+        type: String,
+        required:true,
+        trim:true,
+        unique:true,
+        validate(value)
+        {
+            if(!validator.isEmail(value))
+             throw new Error('invalid email')
+        }
+    },
+    tokens: [{
+        token:{
+            type:String,
+            required:true
+        }
+    }],
+    
+},
+{timestamps: true})
+
+userschema.methods.toJSON=function(){
+    const temp= this.toObject()
+    delete temp.tokens
+    delete temp.password
+    return temp
+}
+ userschema.statics.findByCredentials= async (email,password)=> //model method
+{
+    const temp=await user.findOne({email})
+    if(!temp)
+      { 
+          throw new Error('unable to login')
+        }
+          const ismatch = await bcrypt.compare(password,temp.password)
+     if(!ismatch)
+          throw new Error('unable to login')
+    return temp
+}
+userschema.methods.generatetoken = async function(){ //instance method //arrow function is not made since it doesn't has access to this  
+    try{
+     const token= await jwt.sign({_id:this._id.toString()},'thisismynewcourse',{expiresIn:"7 days"})
+     this.tokens=this.tokens.concat({token})
+       this.save()
+     return token
+    }
+    catch(e){
+        console.log(e)
+        return e
+    }
+} 
+userschema.pre('save',async function(next){
+
+    if(this.isModified('password'))
+       this.password= await bcrypt.hash(this.password,8)
+    next()
+})
+userschema.pre('remove',async function(next){
+    await task.deleteMany({owner:this._id})
+    next()
+})
+const user= mongoose.model('users',userschema)
+module.exports=user
