1aa8353b9c78d508c5f8018853825b42d04cff44
Ankit Pathak <ankit.pathak.1500@gmail.com>
2022-04-23 16:10:25 +0530

    minor changes

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Profile from './components/profile';
+import UserProfile from './components/UserProfile';
 import { Routes ,Route } from 'react-router-dom';
 import './App.css';
 
@@ -10,7 +11,8 @@ function App() {
       
         <Routes>
         <Route path = "/" element = {<Profile/>}/>
-        <Route path = "/profile" />
+        
+        <Route path="/user/:user_id" element = {<UserProfile Name = {"Ankit"} Phone_no={6203984762} email = {"ankit.pathak.1500@gmail.com"} Age = {"21"} Blood_group = {"B+"} location = "abcd" />}/>
         </Routes>
      
       
