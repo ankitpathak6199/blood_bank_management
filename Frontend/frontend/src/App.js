65fcc95ec35435591c81cfd11975924b0e2b3910
Ankit Pathak <ankit.pathak.1500@gmail.com>
2022-04-23 11:19:32 +0530

    profile_frontend_basics

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Profile from './profile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       
+      <Profile/>
     </div>
   );
 }
